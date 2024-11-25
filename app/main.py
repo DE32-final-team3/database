f3c1981d7a325d09626e437e69831efdf670f849
thephunkmonk <andrewchoi1218@gmail.com>
2024-11-25 17:48:30 +0900

    trials

--- app/main.py
+++ app/main.py
@@ -38,9 +38,15 @@ from sqlalchemy.orm import Session
 from database import get_db
 from schemas import TrackBase, TrackAudioFeatures
 from crud import insert_track_info, update_audio_features
+from models import Track
 
 app = FastAPI()
-
+@app.on_event("startup")
+async def startup():
+    # This ensures the tables are created on app startup
+    from database import engine
+    # Make sure the tables are created
+    Base.metadata.create_all(bind=engine)
 # Track 기본 정보를 저장하는 엔드포인트
 @app.post("/tracks/")
 def create_tracks(tracks: list[TrackBase], db: Session = Depends(get_db)):
