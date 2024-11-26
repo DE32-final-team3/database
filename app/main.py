46510d8c4777a9c391b41f9269c1926f4a7b3000
thephunkmonk <andrewchoi1218@gmail.com>
2024-11-26 11:39:44 +0900

    edits to main.py

--- app/main.py
+++ app/main.py
@@ -1,41 +1,6 @@
-"""
-from database import engine, SessionLocal
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from models import Base
-from crud import create_track, update_track_details
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
-
-# 데이터베이스 테이블 생성
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
-# 의존성으로 데이터베이스 세션 제공
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.post("/tracks/")
-def add_track(track_data: dict, audio_features: dict = None, db: Session = Depends(get_db)):
-    return create_track(db, track_data, audio_features)
-
-@app.put("/tracks/{track_id}")
-def update_track(track_id: str, track_details: dict, db: Session = Depends(get_db)):
-    return update_track_details(db, track_id, track_details)
-"""
-
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from database import get_db
+from database import engine, get_db
 from schemas import TrackBase, TrackAudioFeatures
 from crud import insert_track_info, update_audio_features
 from models import Base, Track
@@ -44,7 +9,6 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup():
     # This ensures the tables are created on app startup
-    from database import engine
     # Make sure the tables are created
     Base.metadata.create_all(bind=engine)
 # Track 기본 정보를 저장하는 엔드포인트
